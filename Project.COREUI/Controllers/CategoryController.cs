a91c0cc83e73675cb33185cbdc0b17653f652c0d
Sefer Ersan Aydin <ersanaydin@outlook.com>
2023-08-31 12:56:43 +0300

    Category CRUD done

---
+++ Project.COREUI/Controllers/CategoryController.cs
@@ -0,0 +1,61 @@
+﻿using Castle.DynamicProxy.Generators.Emitters.SimpleAST;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Project.BLL.ManagerServices.Abstracts;
+using Project.COREUI.VMClasses;
+using Project.ENTITIES.Models;
+
+namespace Project.COREUI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        ICategoryManager _cMan;
+
+        public CategoryController(ICategoryManager cMan)
+        {
+            _cMan = cMan;
+        }
+        public IActionResult CategoryList()
+        {
+            CategoryVM cvm = new CategoryVM()
+            {
+                Categories = _cMan.GetActives(),
+            };
+            return View(cvm);
+        }
+
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddCategory(Category category)
+        {
+            TempData["message"] = _cMan.Add(category);
+            return RedirectToAction("CategoryList");
+        }
+
+        public IActionResult UpdateCategory(int id)
+        {
+            CategoryVM cvm = new CategoryVM()
+            {
+                Category = _cMan.Find(id)
+            };
+            return View(cvm);
+        }
+        [HttpPost]
+
+        public IActionResult UpdateCategory(Category category)
+        {
+            _cMan.Update(category);
+            return RedirectToAction("CategoryList");
+        }
+
+        public ActionResult DeleteCategory(int id) 
+        { 
+            _cMan.Delete(_cMan.Find(id));
+            return RedirectToAction("CategoryList");
+        }
+    }
+}
---
+++ Project.COREUI/VMClasses/CategoryVM.cs
@@ -0,0 +1,11 @@
+﻿using Project.ENTITIES.Models;
+using System.Collections.Generic;
+
+namespace Project.COREUI.VMClasses
+{
+    public class CategoryVM
+    {
+        public Category Category { get; set; }
+        public List<Category> Categories { get; set; }
+    }
+}
